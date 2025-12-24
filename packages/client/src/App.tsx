d0ac81734d4a34e43ba35787a9261412c94aa47a
andreassosilo <andreas.sosilo@gmail.com>
2025-12-24 14:08:59 +0100

    Testing if Husky is working

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,9 +13,9 @@ function App() {
    return (
       <div className="p-4">
          <p className="font-bold text-3xl">{message}</p>
-           <Button variant={'outline'}>Click me!</Button>
+             <Button variant={'outline'}>Click me!</Button>
       </div>
-   );
+   )
 }
 
 export default App;
