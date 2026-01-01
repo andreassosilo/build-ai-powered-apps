351aebfd6c296d2d4dcaaa82e6af18d962faad56
andreassosilo <andreas.sosilo@gmail.com>
2026-01-01 13:51:50 +0100

    Refactoring the API layer & fix frontend issue

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,9 +1,9 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'react-loading-skeleton/dist/skeleton.css';
-import './index.css';
 import App from './App.tsx';
+import './index.css';
 
 const queryClient = new QueryClient();
 
